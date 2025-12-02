856378e1db32dcecd3233348b07b1cf3b2ad8278
iQuanta Edu <146808539+iquantain@users.noreply.github.com>
2025-12-02 17:40:07 +0530

    new file

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="banner">
+      <h1 className="text-center text-[72px]">Hello Alimam Miya</h1>
+    </div>
+  );
+}
